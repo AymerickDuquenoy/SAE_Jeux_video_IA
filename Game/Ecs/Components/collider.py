5b40238271058839280385d871401de8a593867d
Manuki17 <manonchagot17@gmail.com>
2025-10-10 10:30:46 +0200

    Nom du jeu ASCII dans README, ajout de trois components

--- Game/Ecs/Components/collider.py
+++ Game/Ecs/Components/collider.py
@@ -1,21 +1,15 @@
-"""
-===========================================================================================================================
-= SAE Jeux Vidéo Sept. - Oct. 2025                                                                                        =
-= BUT 3 APP - IUT de Calais                                                                                               =
-= Auteurs : CHAGOT Manon, CONDETTE Albin, DELHAYE Florian, DOUILLY Quentin, DUQUENOY Aymerick                             =                                                            
-===========================================================================================================================                                                                             
-"""
-
 from dataclasses import dataclass
 from typing import Tuple
 
 @dataclass
 class Collider:
     """
-    Component Collider.
-    Définit une forme de collision simple pour les entités.
+    Component Collider. Définit une forme de collision simple pour les entités.
+    
+    Attributes:
+        size: Taille (width, height) pour les boîtes (Tuple[float, float]).
     """
-    size: Tuple[float, float] = None  # (width, height) de la box
+    size: Tuple[float, float] = None
 
     def __post_init__(self):
         # S'assurer que size est défini pour les box
