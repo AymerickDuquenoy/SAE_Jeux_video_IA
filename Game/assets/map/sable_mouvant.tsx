956ca3098b70c23337a0e5581e0693e660db12e8
Albin condette <condettealbin@gmail.com>
2025-10-20 14:02:53 +0200

    map

---
+++ Game/assets/map/sable_mouvant.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sable_mouvant" tilewidth="32" tileheight="32" tilecount="54" columns="9">
+ <image source="../textures/quicksan_texture.png" width="309" height="206"/>
+ <tile id="20">
+  <properties>
+   <property name="sables_mouvants" value=""/>
+  </properties>
+ </tile>
+</tileset>
