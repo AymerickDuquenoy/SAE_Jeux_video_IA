8c63791556e1436b037a767f72c538096423f7c9
Albin condette <condettealbin@gmail.com>
2026-01-27 22:42:30 +0100

    Ajout_commentaire_Components

--- Game/Game/Ecs/Components/unitStats.py
+++ Game/Game/Ecs/Components/unitStats.py
@@ -11,13 +11,16 @@ class UnitStats:
     armor: float = 5.0      # Niveau de blindage
     cost: float = 100.0     # Coût de l'unité
 
+    """Retourne une chaîne de caractères représentant l'objet."""
     def __str__(self):
         return (f"UnitStats(speed={self.speed}, power={self.power}, "
                 f"armor={self.armor}, cost={self.cost})")
 
+    """Overloading de repr pour faciliter le debugging"""
     def __repr__(self):
         return self.__str__()
 
+    """Retourne un dictionnaire représentant l'objet."""
     def to_dict(self):
         return {
             "speed": self.speed,
